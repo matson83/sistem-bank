956a86cdc3dba2f89a28ae288ebd077e0743bd69
matson83 <matsonjunior194@gmail.com>
2025-09-20 10:19:37 -0300

    Feature: Creating Component Login Template Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/components/navbar/navbar";
+import { LoginForm } from './pages/auth/login-form/login-form';
+import { LoginTemplate } from './pages/auth/login-template/login-template';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [LoginForm, LoginTemplate],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
